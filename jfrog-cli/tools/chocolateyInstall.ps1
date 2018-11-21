621122d523360fdd136d60d29a2131073e752b98
Justin Georgeson <jgeorgeson@lopht.net>
2018-11-20 23:57:51 -0600

    jfrog-cli: set 64bit checksum to sha256 in packageArgs

--- jfrog-cli/tools/chocolateyInstall.ps1
+++ jfrog-cli/tools/chocolateyInstall.ps1
@@ -6,8 +6,8 @@ $packageArgs = @{
   packageName            = 'jfrog-cli'
   fileType               = 'EXE'
   url64bit               = 'https://bintray.com/jfrog/jfrog-cli-go/download_file?file_path=1.21.1%2Fjfrog-cli-windows-amd64%2Fjfrog.exe'
-  checksum64             = '33DB1F822AD1832C539ADA7796BDE663D9361C06AA225E0849641530E3BB7D8D1A79DA051B7F77F17DBBBA89DF9B7FF3CCEC15EA3DCE823FC74E768E9EBF7A9B'
-  checksumType64         = 'sha512'
+  checksum64             = '019f66f78d5be1ae16d9519e916709a1c22ffaddbdf7c06416f9e76b9a4b9f4b'
+  checksumType64         = 'sha256'
   silentArgs             = ''
   validExitCodes         = @(0)
   softwareName           = 'jfrog-cli'
