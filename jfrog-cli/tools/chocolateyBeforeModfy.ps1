9607fe99cbe1044480a3e7ac1f9701bee8bc13f2
Justin Georgeson <jgeorgeson@lopht.net>
2018-11-20 23:31:23 -0600

    jfrog-cli: Log removal of legacy  path during upgrade/uninstall

--- jfrog-cli/tools/chocolateyBeforeModfy.ps1
+++ jfrog-cli/tools/chocolateyBeforeModfy.ps1
@@ -1,11 +1,8 @@
 ﻿$jfrogDir = "$env:ProgramData\Jfrog"
-if(Test-Path -Path "$jfrogDir\jfrog.exe" -ErrorAction SilentlyContinue)
+if(Test-Path -Path "$jfrogDir" -ErrorAction SilentlyContinue)
 {
+    Write-Warning ("Removing legacy directory '$jFrogDir'.")
     Remove-Item -Path $jfrogDir -Force -Recurse > $null
-}
-else
-{
-    Write-Warning ("Cannot locate jfrog.exe at $jFrogDir.")
 }
 
 $jfrogDir = $jfrogDir -replace '\\','\\' #this formats the path to work with -replace
@@ -13,12 +10,9 @@ $path     = "Registry::HKEY_LOCAL_MACHINE\SYSTEM\CurrentControlSet\Control\Sessi
 $oldPath  = (Get-ItemProperty -Path $path -Name PATH).Path
 if($oldPath | Select-String -Pattern 'jfrog')
 {
+    Write-Warning ("Removing legacy PATH entry '$jFrogDir'.")
     $newPath  = $oldPath -replace ";$jfrogDir\\",";"
     Set-ItemProperty -Path $path -Name PATH –Value $newPath
 
     &"C:\ProgramData\Chocolatey\bin\RefreshEnv.cmd"
-}
-else
-{
-    Write-Warning ('Cannot locate jfrog in PATH variable.')   
 }
